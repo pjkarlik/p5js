68c9a8e59bfcf672bbcb2f1a70734b135ae47a28
pjkarlik <pjkarlik@hotmail.com>
2017-03-31 19:55:01 -0400

    need to clean up all these renders

--- src/components/VrtRender.js
+++ src/components/VrtRender.js
@@ -15,11 +15,7 @@ const sketch = function (p) {
 
   var amountA;
   var amountB;
-  var gridY;
-  var multX;
-  var multY;
-  var speed;
-  var gameTime = .05;
+  var amountC;
 
   var mouseX = 0;
   var mouseY = 0;
@@ -39,6 +35,7 @@ const sketch = function (p) {
   p.setOptions = function(options) {
     amountA = options.amountA || amountA;
     amountB = options.amountB || amountB;
+    amountC = options.amountC || amountC;
   }
 
   p.windowResized = function() {
@@ -60,29 +57,44 @@ const sketch = function (p) {
     // var ang1 = p.TWO_PI * p.noise(0.01*p.frameCount + 10);
     // var ang2 = p.TWO_PI * p.noise(0.01*p.frameCount + 20);
     var ang3 = (0.01*p.frameCount + 30);
-    var rx = 255 * Math.cos(p.frameCount*0.7 * p.PI / 180);
-    var tx = 255 * Math.sin(p.frameCount * p.PI / 180);
-    var size1 = 100 * p.noise(0.01*p.frameCount + 90);
+    var rx = 255 * Math.cos(p.frameCount*0.7 * p.PI / 360);
+    var tx = 255 * Math.sin(p.frameCount * p.PI / 360);
+    var dx = 100 * Math.sin(p.frameCount*0.5 * p.PI / 360);
+    var size1 = 50 * p.noise(0.01*p.frameCount + 50);
     // var size2 = 50 * p.noise(0.01*p.frameCount + 60);
 
     var size = size1;
-
-    p.translate(width_half, height_half);
+    var color = Math.abs(100 * Math.sin(p.frameCount*0.2 * p.PI / 360));
+    var color2 = Math.abs(100 * Math.cos(p.frameCount*0.1 * p.PI / 360));
+    // tick++;
+    // if (tick > 10){ tick=0; console.log(color,color2);}
+    p.ellipseMode(p.CENTER);
+    p.translate(width_half + rx, height_half + tx);
     p.rotate(ang3);
     for (var i = 0; i < amountA; i++) {
-      p.fill(i * 10,100,100);
+      p.fill(color,100,100);
       p.push();
       p.rotate(p.TWO_PI * i / amountA);
       p.translate(tx, -size/2);
-      p.rect(0, 0, size, size);
+      p.ellipse(0, 0, size, size);
       p.translate(size/2, size/2);
       p.rotate(ang3);
       for (var j = 0; j < amountB; j++) {
         p.push();
         p.rotate(p.TWO_PI * j / amountB);
         p.translate(rx, -size/4);
-        p.fill(j * 10,100,100);
-        p.rect(0, 0, size/2, size/2);
+        p.fill(color2,100,100);
+        p.ellipse(0, 0, size/2, size/2);
+        p.translate(size/2, size/2);
+        p.rotate(ang3 * 2);
+        for (var c = 0; c < amountC; c++) {
+          p.push();
+          p.rotate(p.TWO_PI * c / amountC);
+          p.translate(dx, -size/6);
+          p.fill(color - c * 10 ,100,100);
+          p.ellipse(0, 0, size/3, size/3);
+          p.pop();
+        }
         p.pop();
       }
       p.pop();
@@ -95,8 +107,7 @@ const sketch = function (p) {
   }
 
   p.viewPort = function() {
-    p.blendMode(p.DIFFERENCE);
-    p.background(0,0,20,15);
+    p.background(0,0,10,1);
   }
 
 };
@@ -122,20 +133,26 @@ export default class Render {
   };
   createGUI = () => {
     this.options = {
-      amountA: 14,
-      amountB: 5,
+      amountA: 5,
+      amountB: 3,
+      amountC: 2,
     };
     this.gui = new dat.GUI();
     const folderRender = this.gui.addFolder('Render Options');
 
-    folderRender.add(this.options, 'amountA', 1, 20).step(1)
+    folderRender.add(this.options, 'amountA', 1, 10).step(1)
       .onFinishChange((value) => {
         this.options.amountA = value;
         this.setOptions(this.options);
       });
-    folderRender.add(this.options, 'amountB', 1, 20).step(1)
+    folderRender.add(this.options, 'amountB', 1, 10).step(1)
       .onFinishChange((value) => {
         this.options.amountB = value;
+        this.setOptions(this.options);
+      });
+    folderRender.add(this.options, 'amountC', 1, 10).step(1)
+      .onFinishChange((value) => {
+        this.options.amountC = value;
         this.setOptions(this.options);
       });
     folderRender.open();
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Render from './components/VrtRender';
+import Render from './components/GlyRender';
 import { description, version } from '../version.json';
 require('../resources/styles/styles.css');
 
