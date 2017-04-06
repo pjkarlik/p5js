0242a2f0ecbff7da7f991fe6dacbfcd3aaf583d2
pjkarlik <pjkarlik@hotmail.com>
2017-04-06 19:34:53 -0400

    need to remove other files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Render from './components/GlyRender';
+import Render from './components/QuadRender';
 import { description, version } from '../version.json';
 require('../resources/styles/styles.css');
 
